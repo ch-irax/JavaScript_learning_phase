27c4c6c3ff76167f4b16c1a66f1a3087d1cd000a
ch-irax <chiragpsharma2206@gmail.com>
2026-04-17 14:55:00 +0530

    Initial commit

---
+++ Chai aur Code/data_types_ECMAscript.js
@@ -0,0 +1,24 @@
+"use strict"; //treats JS  code as newer version
+// but latest versions consider it automatically
+
+// alert("3+9") in console
+
+let name = "Mav"
+let age = 20
+let isLoggedIN = false
+
+// Data_Types
+// Number , bigint(rare)
+// String => " "
+// Boolean => true or false
+// null -> standalone value [representation of empty value]
+// undefined -> variable declared but no value assigned 
+// symbol => used in react to find uniqueness
+
+// non permitive
+// Object
+// console.log(typeof variable_name) to find the type of variable
+
+console.log(typeof null)    //type of null is object
+console.log(typeof undefined)  // type is undefined
+
---
+++ arrays&strings.js
@@ -0,0 +1,7 @@
+// let arr = [1, 2, 3, 5, 6, 8]
+
+// console.log(arr)
+// console.log(arr.length)
+
+a = "vengence"
+console.log(a.toUpperCase())

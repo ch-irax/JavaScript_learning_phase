2713e1b492b53fb67da664bd424ef7b56ea4b2af
ch-irax <ch.irax000@gmail.com>
2026-04-25 17:12:53 +0530

    all changes

--- functions.js
+++ functions.js
@@ -28,8 +28,30 @@ function calculateCart(val1, val2, ...num1) {   //rest operator (... ) returns a
     return num1
 }
 
-console.log(calculateCart(100, 200, 300, 3323, 3, 3434)); //val1=100 , val2=200
+// console.log(calculateCart(100, 200, 300, 3323, 3, 3434)); //val1=100 , val2=200
 
-const name ={
-    
+const test_object = {
+    name: "Mav",
+    age: 22,
+    id: "26MVK220"
 }
+
+function handleobject(any_object) {
+    console.log(`Name ${any_object.name}, age ${any_object.age}, id ${any_object.id}`);
+}
+// handleobject(test_object)
+
+// object can be passed on function call
+// handleobject({
+//     name:"newuser",
+//     age:33,
+//     id:"sdadwee32"
+// })
+ 
+let myArray = [100,300,500,700,900]
+
+function fetchSecond(anyArray){
+    return anyArray[1]
+}
+
+console.log(fetchSecond(myArray));

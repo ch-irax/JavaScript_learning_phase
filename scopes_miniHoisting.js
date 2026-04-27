8b40b86f5366317e7c9ef4ab3ab52fb8bd840861
ch-irax <ch.irax000@gmail.com>
2026-04-27 18:32:58 +0530

    final commit with comments

--- scopes_miniHoisting.js
+++ scopes_miniHoisting.js
@@ -4,7 +4,7 @@ if (true) {
     let a = 10 // local scope
     const b = 20
     // var c = 30
-    console.log(`Inner variable ${a}`);
+    // console.log(`Inner variable ${a}`);
 }
 
 // console.log(a);
@@ -24,4 +24,16 @@ function first() {
     second()
 }
 
-first( )
+// first( )
+
+if (true) {
+    const username = "Mav"
+    if (username === "Mav") {
+        const age = "22"
+        console.log(username + age);
+    }
+    // console.log(age); will throw an error as age's scope is local
+}
+
+// console.log(username);  will throw an error as username's scope is local
+// A function cannot be accessed before drclaration. 

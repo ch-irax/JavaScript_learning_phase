7ec7fca80e879a84434106ba481148c0bb6531e9
ch-irax <ch.irax000@gmail.com>
2026-05-02 22:01:50 +0530

    for_loops

---
+++ Iterations/1.js
@@ -0,0 +1,48 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best ");
+
+    }
+    // console.log(element);
+
+}
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop ${i}`);
+
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner value${j} `);
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+
+}
+
+
+let myArr = [`sups`, `batman`, `flash`]
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    // console.log(element);
+}
+
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        // console.log("detected 5");
+        break  // stops the control flow completely
+    }
+    // console.log(`Vslue of i ${i}`);
+
+}
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("detected 5");
+        continue  // stopps the control flow once
+    }
+    console.log(`Vslue of i ${i}`);
+
+}

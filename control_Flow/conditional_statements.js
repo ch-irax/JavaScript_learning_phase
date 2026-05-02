b008f4225e5b926b662e24344ef513c58e3e2746
ch-irax <ch.irax000@gmail.com>
2026-05-02 20:19:10 +0530

    various conditional scenarios

---
+++ control_Flow/conditional_statements.js
@@ -0,0 +1,25 @@
+// let balance = 1000
+
+// if (balance <= 500) {
+//     console.log("less");
+// } else if (balance > 1000) {
+//     console.log("more");
+
+// } else if (balance == 1000) {
+//     console.log("maintained");
+// }
+
+let token = 500
+
+// if (token >= 500) {
+//     const power = "fly"
+//     console.log(`Power granted: ${power}`);
+
+// }
+// console.log(`Power granted: ${power}`);  scope of the variable power is local 
+
+// Single statement can be executed in the same line 
+let limit = 100
+if (limit < token) console.log("Limit achieved"), console.log("Try after 10hrs");
+
+

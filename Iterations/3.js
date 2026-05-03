725fbbd55460717005762482eb5fe1055c2b03c2
ch-irax <ch.irax000@gmail.com>
2026-05-03 18:15:35 +0530

    for of loop

--- Iterations/3.js
+++ Iterations/3.js
@@ -7,13 +7,41 @@
 const arr = [1, 2, 3, 4, 5, 6]
 
 for (const val of arr) {
-    console.log(val);
+    // console.log(val);
 
 }
 
 const str = "MAVERICK"
 
 for (const lett of str) {
-    console.log(`Seperate value ${lett}`);
-    
-} 
+    // console.log(`Seperate value ${lett}`);
+
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+map.set('IN', "India")  // map takes only unique values, keeps order same
+
+// console.log(map);
+
+for (const [key, value] of map) {  // [key,value] this method is used to seperate the key - value pairs
+    // console.log(key, ':-', value);
+
+}
+
+
+const myobj = {
+    game1: "GTA",
+    game2: "Gta5"
+}
+
+
+// for of is not applicable for objects
+
+// for (const [key, value] of myobj) {
+//     console.log(key, ':-', value);
+
+// }
+

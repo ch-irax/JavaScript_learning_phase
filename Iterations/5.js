7625186544f1b71e03c67ac31e23056ee8ec086f
ch-irax <ch.irax000@gmail.com>
2026-05-03 22:05:52 +0530

    foreach loop - arrays , object in arrays

---
+++ Iterations/5.js
@@ -0,0 +1,43 @@
+const coding = ["js", "python", "cpp", "ruby", "go", "typescript"]
+
+// coding.forEach(function (value) {
+//     console.log(value);
+
+// })  
+
+// coding.forEach((val) => {
+//     console.log(val);
+
+// })
+
+// function printme(item) {
+//     console.log(item);
+
+// }
+
+// coding.forEach(printme)
+
+coding.forEach((values, index, arr) => {
+    // console.log(values, index, arr);
+
+})
+
+const mycode = [
+    {
+        language: "Js",
+        use: "dev"
+    },
+    {
+        language: "python",
+        use: "script"
+    },
+    {
+        language: "c++",
+        use: "embedded "
+    }
+]
+
+mycode.forEach((item) => {
+    console.log(item.language);
+
+});

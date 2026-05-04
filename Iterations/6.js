1d43b44345c318d2d660289ab9e5eb8f59e8e0f4
ch-irax <ch.irax000@gmail.com>
2026-05-04 21:50:28 +0530

    callback

--- Iterations/6.js
+++ Iterations/6.js
@@ -1,9 +1,24 @@
 const coding = ["js", "python", "cpp", "ruby", "go", "typescript"]
 
-const val = coding.forEach((item) => {
-    console.log(item);
-    return item
+// const val = coding.forEach((item) => {
+// console.log(item);
+// return item
 
+// })
+
+// console.log(val);
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const n_nums = nums.filter((val) => {
+//     return val > 4
+// })
+
+const arr = []
+nums.forEach((item) => {
+    if (item > 4) {
+        arr.push(item)
+    }
 })
+console.log(arr);
 
-console.log(val);

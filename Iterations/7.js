1b45059d5cb5298f0f2903cfc880c59f9c4e94f1
ch-irax <ch.irax000@gmail.com>
2026-05-05 12:45:31 +0530

    map method summarized

--- Iterations/7.js
+++ Iterations/7.js
@@ -1,5 +1,16 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-const newNums = myNumbers.map((val) => { return val + 10 })
-console.log(newNums);
+// const newNums = myNumbers.map((val) => { return val + 10 })
+// console.log(newNums);
+
+// const val = myNumbers.forEach((item) => {
+//     console.log(item + 10);
+
+// })
+// console.log(val);
+
+const nums = myNumbers.map((val) => { return val * 10 }).map((val) => { return val + 1 }).filter((val) => { return val >= 40 })
+
+console.log(nums);
+

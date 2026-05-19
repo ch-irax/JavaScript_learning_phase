ee2590fefd5c8297075c683272ce1229434d61c3
chirag <ch.irax000@gmail.com>
2026-05-19 10:11:51 +0530

    Promises

---
+++ Promises.js
@@ -0,0 +1,23 @@
+const promise_one = new Promise(function (resolve, reject) {
+    //Do an async task
+    //DB calls, cryptography, network
+    setTimeout(function () {
+        console.log("Async task completed");
+        resolve()
+    }, 1000)
+})
+
+promise_one.then(function () {
+    console.log("Promise consumed");
+    
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task two");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("async 2 resolved");
+    
+})
